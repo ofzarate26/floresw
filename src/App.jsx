1f7145537c3c2259d0c24851f9b020448e022860
kevin-riveros <riverosk885@gmail.com>
2023-09-21 01:07:59 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+
+function getQueryParamValue(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
+
+function App() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const name = getQueryParamValue("novia");
+    if (name) {
+      document.title = `Flores para ${name}`;
+      setName(name);
+    }
+  }, []);
+
+  return (
+    <>
+      {
+        name && <h1>Flores amarillas para mi novia {name}</h1>
+      }
+
+      <div className="flower">
+        <div className="mid"></div>
+        <div className="Petal1 p1"></div>
+        <div className="Petal1 p2"></div>
+        <div className="Petal1 p3"></div>
+        <div className="Petal1 p4"></div>
+        <div className="Petal2 p1"></div>
+        <div className="Petal2 p2"></div>
+        <div className="Petal2 p3"></div>
+        <div className="Petal2 p4"></div>
+        <div className="Petal3 p1"></div>
+        <div className="Petal3 p2"></div>
+        <div className="Petal3 p3"></div>
+        <div className="Petal3 p4"></div>
+      </div>
+      <div className="flower-container">
+        <div className="leaf-left"></div>
+        <div className="leaf-right"></div>
+        <div className="stem"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
